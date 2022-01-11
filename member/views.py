79c3ec86e71868f856ee22f9440c9f3e6f8761d4
Jin-devST <kpasd002@gmail.com>
2022-01-11 23:04:39 +0900

    멤버 모델 생성

--- member/views.py
+++ member/views.py
@@ -1,12 +1,16 @@
 # from django.shortcuts import render
 from .models import Member
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
 # Create your views here.
-from .serializer import MemberSerializer
+from .serializer import MemberListSerializer, MemberCreateSerializer
 
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
-    serializer_class = MemberSerializer
+    serializer_class = MemberListSerializer
 
+
+# class MemberCreate(generics.CreateAPIView):
+#     queryset = Member.objects.all()
+#     serializer_class = MemberCreateSerializer
