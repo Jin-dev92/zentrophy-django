9767971bd465a45c5359429853ef45e3cafe237b
Jin-devST <kpasd002@gmail.com>
2022-01-13 14:50:38 +0900

    post 앱  기본 설정

--- post/models.py
+++ post/models.py
@@ -1,3 +1,18 @@
+import time
+from enum import Enum
+
 from django.db import models
 
+
+class PostType(Enum):
+    FAQ = 0,
+    NOTICE = 1,
+
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField(max_length=200)
+    type = PostType
+    is_created = models.DateTimeField(default=time.localtime())
