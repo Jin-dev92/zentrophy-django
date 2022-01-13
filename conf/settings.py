83df50774ec69a91df9d2bbcc48f15737f4d7642
Jin-devST <kpasd002@gmail.com>
2022-01-13 11:03:56 +0900

    todo before adjust ninja api

--- conf/settings.py
+++ conf/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # rest_framework
-    'rest_framework',
-    'rest_framework_swagger',
+    # 'rest_framework',
+    # 'rest_framework_swagger',
     # server-app
     'member',
 ]
--- member/views.py
+++ member/views.py
@@ -1,15 +1,2 @@
 # from django.shortcuts import render
 from .models import Member
-from rest_framework import viewsets
-
-# Create your views here.
-from .serializer import MemberListSerializer
-
-
-class MemberViewSet(viewsets.ModelViewSet):
-    queryset = Member.objects.all()
-    serializer_class = MemberListSerializer
-
-# class MemberCreate(generics.CreateAPIView):
-#     queryset = Member.objects.all()
-#     serializer_class = MemberCreateSerializer
