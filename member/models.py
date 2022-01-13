c373db9ae5b9e1d0bd916c66ac1413ea4a5475e3
김의진 <camel@lizhko.io>
2022-01-13 10:58:43 +0900

    브랜치 따기 전 커밋

--- member/models.py
+++ member/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Create your models here.
 class Member(models.Model):
     user_name = models.CharField(max_length=200)
-    user_mail = models.EmailField(max_length=200)
+    user_mail = models.EmailField(max_length=200, unique=True)
     phone_number = models.CharField(max_length=12)
     address = models.CharField(max_length=200)
