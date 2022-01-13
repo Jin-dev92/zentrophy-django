efb5ba009601aa6bd4bbfed465026815dbbe1e61
Jin-devST <kpasd002@gmail.com>
2022-01-13 10:58:43 +0900

    브랜치 따기 전 커밋

--- member/views.py
+++ member/views.py
@@ -1,15 +1,14 @@
 # from django.shortcuts import render
 from .models import Member
-from rest_framework import viewsets, generics
+from rest_framework import viewsets
 
 # Create your views here.
-from .serializer import MemberListSerializer, MemberCreateSerializer
+from .serializer import MemberListSerializer
 
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberListSerializer
-
 
 # class MemberCreate(generics.CreateAPIView):
 #     queryset = Member.objects.all()
