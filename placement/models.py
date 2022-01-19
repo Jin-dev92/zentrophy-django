87185e79412914d646468454e3dc9fb5cf200642
김의진 <camel@lizhko.io>
2022-01-19 15:04:16 +0900

    플레이스 모델 작옵

--- placement/models.py
+++ placement/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from placement.constant import PlacementType, OperationState
+
+
+class Placement(models.Model):
+    id = models.AutoField(primary_key=True)
+    placement_name = models.CharField(max_length=100, blank=True, null=False)
+    placement_owner = models.CharField(max_length=20, blank=True, null=False)
+    placement_address = models.CharField(max_length=100, blank=True, null=False)
+    placement_type = models.PositiveSmallIntegerField(choices=PlacementType.choices, default=PlacementType.SERVICE)
+    operation_start = models.CharField(max_length=10, blank=True, null=False)
+    operation_end = models.CharField(max_length=10, blank=True, null=False)
+    operation_state = models.PositiveSmallIntegerField(choices=OperationState.choices, default=OperationState.OPERATING)
+    # file = models.FileField
