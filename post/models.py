7157e9920f32684f1a94851b466020ad7d8af9ef
Jin-devST <kpasd002@gmail.com>
2022-01-23 13:53:18 +0900

    임시저장

--- post/models.py
+++ post/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from post.constant import PostType
-
-
-class TimeStampModel(models.Model):
-    is_created = models.DateTimeField(auto_now_add=True)  # 처음에 추가될때 생성된다.
-    is_updated = models.DateTimeField(auto_now=True)  # 처음 추가 될때 생성이 되면서 데이터가 바뀔때 마다 같이 바뀌게된다.
-
-    class Meta:
-        abstract = True
+from util.util import TimeStampModel
 
 
 class Post(TimeStampModel):
@@ -15,4 +8,4 @@ class Post(TimeStampModel):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.CharField(max_length=200)
-    post_type = models.IntegerField(choices=PostType.choices, default=PostType.NOTICE)
+    post_type = models.PositiveSmallIntegerField(default=PostType.NOTICE)
