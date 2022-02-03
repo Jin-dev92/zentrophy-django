5f143ac5ff663c366c7cf41ccd1a15be37125709
김의진 <camel@lizhko.io>
2022-02-03 16:14:16 +0900

    order get api 추가

--- order/api.py
+++ order/api.py
@@ -1,12 +1,18 @@
 from typing import List
 
 from ninja import Router
-
+from django.shortcuts import get_list_or_404, get_object_or_404
+from order.models import Order
 from order.schema import OrderListSchema
 
 router = Router()
 
 
-@router.get("", description="주문", response={200: List[OrderListSchema]})
+@router.get("", description="전체 주문 조회", response={200: List[OrderListSchema]})
 def get_list_order(request):
-    return "aa"
+    return Order.objects.all()
+
+
+@router.get("", description="id로 특정 주문 조회", response={200: OrderListSchema})
+def get_list_order_by_id(request, id: int):
+    return get_object_or_404(Order, id=id).delete()
