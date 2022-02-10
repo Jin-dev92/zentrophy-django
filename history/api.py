c0f7ae7e888d750103be7b8783c5d106dd406b5d
김의진 <camel@lizhko.io>
2022-02-10 17:10:37 +0900

    나중에 바뀔수도있으므로 라이센스 코드 길이 제한 상수화

--- history/api.py
+++ history/api.py
@@ -1,10 +1,11 @@
 from typing import List
-
+from django.shortcuts import get_object_or_404
 from ninja import UploadedFile, File, Router
 
 from history.models import AfterService
-from history.schema import HistoryListSchema
+from history.schema import HistoryListSchema, AfterServiceInsertSchema
 # from util.number import generate_register_number
+from placement.models import Placement
 from util.number import generate_random_number
 
 history_router = Router()
@@ -13,7 +14,8 @@ after_service_router = Router()
 
 
 @history_router.post("/", description="a/s 내역 생성")
-def create_after_service_history(request):
+def create_after_service_history(request, payload: AfterServiceInsertSchema):
     registration_number = generate_random_number()
+    # place = get_object_or_404(Placement, id=payload.dict()['place_id'])
+    # return AfterService.objects.create(registration_number=registration_number, place=place)
     return registration_number
-    # return AfterService.objects.create(registration_number=registration_number)
--- history/schema.py
+++ history/schema.py
@@ -3,4 +3,7 @@ from ninja import Schema
 
 class HistoryListSchema(Schema):
     id: int = None
-    
+
+
+class AfterServiceInsertSchema(Schema):
+    place_id: int
