f610a96aca5eab4cdb7f5c8f5a803819d66ce7ac
김의진 <camel@lizhko.io>
2022-03-04 15:18:32 +0900

    퍼미션 관련 로직 수정

--- member/api.py
+++ member/api.py
@@ -1,7 +1,5 @@
 from typing import List, Optional
 
-# from django.contrib.auth.decorators import permission_required
-from conf import settings
 from ninja import Router, Form
 from ninja.responses import Response
 
@@ -10,10 +8,10 @@ from member.models import User
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import login, logout, authenticate
 
-# from util.custom_decorator import admin_only
 from util.default import ResponseDefaultHeader
 from util.exception.exception import USER_NOT_ACCESS_DENIED
 from util.params import prepare_for_query
+from util.permission import has_permission
 
 router = Router()
 
@@ -22,6 +20,8 @@ router = Router()
 def get_list_member(request, id: Optional[int] = None, email: Optional[str] = None, username: Optional[str] = None,
                     # sort: Optional[int] = None
                     ):
+    if has_permission(request) is False:
+        raise USER_NOT_ACCESS_DENIED
     params = prepare_for_query(request=request, exceptions=['sort'])
     return User.objects.filter(**params).all().order_by()
 
@@ -47,16 +47,12 @@ def member_login(request, email: str = Form(...), password: str = Form(...)):
     if user is None:
         raise ValueError('아이디 혹은 비밀번호가 틀립니다.')
     login(request, user)
-    # if user is not None:
-    #     login(request, user)
-    # else:
-    #     raise ValueError('아이디 혹은 비밀번호가 틀립니다.')
 
 
 @router.delete("/", description="회원 삭제")
 def delete_user(request, id: int):
     user = request.user
-    if user.groups != settings.ADMIN_GROUP_NAME:  # 어드민인지 확인
+    if has_permission(request) is False:  # 일반 유저 일 때
         target = get_object_or_404(User, id=id)
         if user != target:  # 일반 유저가 다른 유저의 계정을 삭제 하기 못하게 하기 위한 코드
             raise USER_NOT_ACCESS_DENIED
