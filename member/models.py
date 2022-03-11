0ab3f119ba77c139878f08f3fdb877cb9212d126
김의진 <camel@lizhko.io>
2022-03-11 15:49:52 +0900

    member / is_business 제거

--- member/models.py
+++ member/models.py
@@ -56,7 +56,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     address = models.CharField(max_length=200, blank=True)
     address_detail = models.CharField(max_length=200, blank=True)
     zipcode = models.CharField(max_length=20, blank=True)
-    is_business = models.BooleanField(default=False)
+    # is_business = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
