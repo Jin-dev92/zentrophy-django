5b4410606207e95956edd5a1b00b3014c2a1ba75
Jin-devST <kpasd002@gmail.com>
2022-04-19 14:24:51 +0900

    extra_subside admin 추가

--- order/admin.py
+++ order/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from order.models import Order, NecessaryDocumentFile, Subside, IntegratedFeePlan
+from order.models import Order, NecessaryDocumentFile, Subside, IntegratedFeePlan, ExtraSubside
 
 admin.site.register(Order)
 admin.site.register(NecessaryDocumentFile)
 admin.site.register(Subside)
+admin.site.register(ExtraSubside)
 admin.site.register(IntegratedFeePlan)
