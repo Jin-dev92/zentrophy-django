935656089c911d097da96a42ae1a7571a6d9f3cc
Jin-devST <kpasd002@gmail.com>
2022-04-28 21:06:28 +0900

    플레이스 api 마무리

--- placement/api.py
+++ placement/api.py
@@ -1,11 +1,9 @@
-from typing import List, Optional
+from typing import List
 from django.db import transaction
-from django.db.models import Prefetch
 from django.shortcuts import get_object_or_404
 from ninja import Router
 from ninja.files import UploadedFile
 
-from placement.constant import PlacementType
 from placement.models import Placement
 from placement.schema import PlacementListSchema, PlacementInsertSchema
 from util.params import prepare_for_query
@@ -15,12 +13,10 @@ router = Router()
 
 @router.get("/",
             description="플레이스 리스트 가져오기",
-            tags=["place"],
             response={200: List[PlacementListSchema]},
             auth=None
             )
-def get_placement_list_by_type(request, placement_type: PlacementType = PlacementType.SERVICE,
-                               remote_pk: int = None):
+def get_placement_list_by_type(request, remote_pk: int = None):
     params = prepare_for_query(request)
     queryset = Placement.objects.get_queryset(**params)
     return queryset
@@ -28,7 +24,6 @@ def get_placement_list_by_type(request, placement_type: PlacementType = Placemen
 
 @router.get('/{id}', response={200: List[PlacementListSchema]})
 def get_placement_by_id(request, id: int):
-    # queryset = get_object_or_404(Placement, id=id)
     queryset = Placement.objects.get_queryset(id=id)
     return queryset
 
@@ -41,27 +36,10 @@ def create_placement(request, payload: PlacementInsertSchema, id: int = None, fi
         with transaction.atomic():
             payload = payload.dict()
             payload['image'] = file
-            Placement.objects.update_or_create(id=id, default=payload)
+            Placement.objects.update_or_create(id=id, defaults=payload)
     except Exception as e:
         raise e
     return True
-
-
-# @transaction.atomic(using='default')
-# @router.post('/{id}', description="플레이스 정보 수정")
-# def modify_placement_by_id(request, payload: PlacementInsertSchema, id: int, file: UploadedFile = None):
-#     place = get_object_or_404(Placement, id=id, deleted_at__isnull=True)
-#     try:
-#         with transaction.atomic():
-#             pass
-#             # PlacementImage.objects.filter(place=place).delete()
-#             # for image in place.placementimage_set.all():
-#             #     delete_file(image.file)
-#             # Placement.objects.get_queryset(id=id).update(**payload.dict())
-#             # if file:
-#             #     PlacementImage.objects.create(place=place, file=file)
-#     except Exception as e:
-#         raise e
 
 
 @router.delete("/", description="플레이스 삭제")
--- placement/models.py
+++ placement/models.py
@@ -1,22 +1,14 @@
 from django.db import models
 from sorl.thumbnail import ImageField
 
-from placement.constant import PlacementType, OperationState
-from util.models import TimeStampModel, SoftDeleteModel
+from placement.constant import OperationState
+from util.models import SoftDeleteModel
 
 
 class Placement(SoftDeleteModel):
     remote_pk = models.IntegerField(default=0)
     image = ImageField(upload_to="place/%Y/%M", null=True)
     operation_state = models.PositiveSmallIntegerField(default=OperationState.OPERATING)
-
-    # id = models.AutoField(primary_key=True)
-    # placement_name = models.CharField(max_length=100, blank=True, null=False)
-    # placement_owner = models.CharField(max_length=20, blank=True, null=False)
-    # placement_address = models.CharField(max_length=100, blank=True, null=False)
-    # placement_type = models.PositiveSmallIntegerField(default=PlacementType.SERVICE)
-    # operation_start = models.TimeField(null=False)
-    # operation_end = models.TimeField(null=False)
 
     def __str__(self):
         return self.remote_pk
