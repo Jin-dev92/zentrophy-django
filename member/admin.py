0afa75e7cb25b3133c91e9975f37b3e29b5669a2
Jin-devST <kpasd002@gmail.com>
2022-06-02 14:32:44 +0900

    어드민 추가

--- member/admin.py
+++ member/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from member.models import User, MemberOwnedVehicles, RemoteToken
+from member.models import User, MemberOwnedVehicles, RemoteToken, PaymentMethod, Card
 
 admin.site.register(User)
 admin.site.register(MemberOwnedVehicles)
 admin.site.register(RemoteToken)
+admin.site.register(PaymentMethod)
+admin.site.register(Card)
--- order/admin.py
+++ order/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from order.models import Order, DocumentFile, Subside, IntegratedFeePlan, ExtraSubside
+from order.models import Order, DocumentFile, Subside, IntegratedFeePlan, ExtraSubside, CustomerInfo, OrderLocationInfo
 
 admin.site.register(Order)
 admin.site.register(DocumentFile)
 admin.site.register(Subside)
 admin.site.register(ExtraSubside)
 admin.site.register(IntegratedFeePlan)
+admin.site.register(CustomerInfo)
+admin.site.register(OrderLocationInfo)
