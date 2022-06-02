277e4ff9389704be418e8f15d44a867ee22a2876
Jin-devST <kpasd002@gmail.com>
2022-06-02 14:24:17 +0900

    토큰 리모트 어드민 추가

--- member/admin.py
+++ member/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from member.models import User, MemberOwnedVehicles
+from member.models import User, MemberOwnedVehicles, RemoteToken
 
 admin.site.register(User)
 admin.site.register(MemberOwnedVehicles)
+admin.site.register(RemoteToken)
