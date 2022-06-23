bbeda94182559339074fcc03d484de7776ecff66
Jin-devST <kpasd002@gmail.com>
2022-06-23 16:03:16 +0900

    PrevEstimate 어드민 추가

--- history/admin.py
+++ history/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from history.models import Refund, AfterService, Warranty, Cart
+from history.models import Refund, AfterService, Warranty, Cart, PrevEstimate
 
 admin.site.register(Refund)
 admin.site.register(AfterService)
-# admin.site.register(BatteryExchange)
 admin.site.register(Warranty)
 admin.site.register(Cart)
-
+admin.site.register(PrevEstimate)
